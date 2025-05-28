2438a6e81c5b75b279cc364dd08c2432833f9e57
KentaMiyazaki09 <kmiyazaki0924@gmail.com>
2025-05-28 23:39:05 +0900

    フォントサイズをvwに変更

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import styles from './pageTitle.module.scss'
+
 type Props = {
   text: string
 }
 
 export default function PageTitle({ text }: Props) {
   return (
-    <h1>{`${text}`}</h1>
+    <h1 className={styles.pageTitle}>{`${text}`}</h1>
   )
 }
